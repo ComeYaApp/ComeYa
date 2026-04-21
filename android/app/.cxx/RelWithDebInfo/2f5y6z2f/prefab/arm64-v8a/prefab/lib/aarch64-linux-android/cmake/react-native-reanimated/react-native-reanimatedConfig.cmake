09267e451b71ea93f85b9a25a1c708420f9493e1
Caskiuz <rijarwow@gmail.com>
2026-04-21 16:01:30 -0400

    chore: regenerar carpeta android con prebuild - logo rojo ComeYa

---
+++ android/app/.cxx/RelWithDebInfo/2f5y6z2f/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "C:/CY/node_modules/react-native-reanimated/android/build/intermediates/cxx/RelWithDebInfo/4tq2g1z4/obj/arm64-v8a/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/CY/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
