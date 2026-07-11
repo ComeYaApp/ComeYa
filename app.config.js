669727e99562819da3a941d68e6897e06ea101d7
Caskiuz <rijarwow@gmail.com>
2026-07-10 23:06:29 -0400

    fix-remove-expobuildproperties-reanimated-needs-newarch

--- app.config.js
+++ app.config.js
@@ -65,17 +65,6 @@ export default {
     },
     plugins: [
       [
-        "expo-build-properties",
-        {
-          android: {
-            newArchEnabled: true,
-          },
-          ios: {
-            newArchEnabled: false,
-          },
-        },
-      ],
-      [
         "expo-splash-screen",
         {
           image: "./client/assets/splash.png",
