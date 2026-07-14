23dddd8dfdf79dd3bec1ca2e6d9fcd51ed7313e8
Caskiuz <rijarwow@gmail.com>
2026-07-14 10:00:37 -0400

    fix: App Review - DNI opcional, eliminacion cuenta, menu sin registro, build 3

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.comeya.app",
-      buildNumber: "2",
+      buildNumber: "3",
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
       },
