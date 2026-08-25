da181f1a35ab28b4a7af790f8decac446342580d
Caskiuz <rijarwow@gmail.com>
2026-08-25 07:35:48 -0400

    chore(ios): quitar buildNumber manual del app config (lo gestiona autoIncrement remoto de EAS)

--- app.config.js
+++ app.config.js
@@ -14,7 +14,6 @@ export default {
         ios: {
       supportsTablet: true,
       bundleIdentifier: "com.comeya.app",
-      buildNumber: "3",
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
       },
