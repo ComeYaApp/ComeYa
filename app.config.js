c7fcd28774289fc8d082962ccea88d3e6ef85644
Caskiuz <rijarwow@gmail.com>
2026-08-31 18:52:34 -0400

    chore(android): alinear versionCode 20 con la carpeta android para el AAB de Play Store

--- app.config.js
+++ app.config.js
@@ -45,7 +45,7 @@ export default {
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
             package: "com.comeya.app",
-      versionCode: 17,
+      versionCode: 20,
       config: {
         googleMaps: {
           apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
